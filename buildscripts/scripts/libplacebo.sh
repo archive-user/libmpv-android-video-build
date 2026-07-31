79a61b7f3306c5aa2679bece09592f33c6455521
Sle2p <www.766111444@foxmail.com>
2026-07-31 20:09:58 +0800

    Update libplacebo.sh

--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -14,19 +14,13 @@ else
 	exit 255
 fi
 
-unset CC CXX # meson wants these unset
-
+unset CC CXX
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	-Ddemos=false \
-	-Dtests=false \
-	-Dvulkan=disabled \
-	-Dopengl=disabled \
-	-Dd3d11=disabled \
-	-Dglslang=disabled \
-	-Dshaderc=disabled \
-	-Dlcms=disabled \
-	-Ddovi=disabled \
-	-Dlibdovi=disabled
+	-Dvulkan=enabled -Ddemos=false -Dshaderc=enabled -Dlcms=enabled
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
+
+# add missing library for static linking
+# this isn't "-lstdc++" due to a meson bug: https://github.com/mesonbuild/meson/issues/11300
+${SED:-sed} '/^Libs:/ s|$| -lc++|' "$prefix_dir/lib/pkgconfig/libplacebo.pc" -i
